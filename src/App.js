d632105fabc02fec4899d2e7d984fc32d9c6c162
Sagar Singh Solanki <ssolanki@grepruby.io>
2022-04-01 10:53:18 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Link } from "react-router-dom";
+import Product from '../src/components/product'
+
+
+function App() {
+  return (<h1>Hello</h1>);
+}
+
+
+export default App;
